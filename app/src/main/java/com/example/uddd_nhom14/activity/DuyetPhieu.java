dfbf9d8657574f1221d5f27a151e11ecf935f1a8
Nguyen Ai Dan <dan3366881@gmail.com>
2024-06-05 18:42:26 +0700

    add file zzzz

--- app/src/main/java/com/example/uddd_nhom14/activity/DuyetPhieu.java
+++ app/src/main/java/com/example/uddd_nhom14/activity/DuyetPhieu.java
@@ -1,9 +1,6 @@
 package com.example.uddd_nhom14.activity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,22 +10,15 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.uddd_nhom14.R;
 
-public class MenuQuanTri extends AppCompatActivity {
+public class DuyetPhieu extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_menu_quan_tri);
-        Button duyet = findViewById(R.id.btnXemVaDuyetPhieuDangKy);
+        setContentView(R.layout.activity_duyet_phieu);
 
-        duyet.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent I = new Intent(MenuQuanTri.this,DuyetPhieu.class);
-                startActivity(I);
-            }
-        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
