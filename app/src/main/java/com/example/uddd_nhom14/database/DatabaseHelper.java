d072df2f63472d036952ee2192c7841765990a90
huyendieu909 <hd911910@gmail.com>
2024-06-07 12:39:37 +0700

    .

--- app/src/main/java/com/example/uddd_nhom14/database/DatabaseHelper.java
+++ app/src/main/java/com/example/uddd_nhom14/database/DatabaseHelper.java
@@ -7,6 +7,8 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import com.example.uddd_nhom14.entity.Account;
+import com.example.uddd_nhom14.entity.Asset;
+import com.example.uddd_nhom14.entity.Profile;
 import com.example.uddd_nhom14.entity.Rent;
 import com.example.uddd_nhom14.entity.Room;
 
@@ -14,7 +16,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "mydatabase.db";
     private static final int DATABASE_VERSION = 1;
 
-//Thông số bảng account
+    //Thông số bảng account
     public static final String ACCOUNT_TABLE_NAME = "users";
     public static final String COLUMN_ID = "id";
     public static final String COLUMN_USERNAME = "username";
@@ -22,7 +24,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String COLUMN_NAME = "name";
     public static final String COLUMN_ROLE = "role";
 
-//Thông số bảng rooms
+    //Thông số bảng rooms
     public static final String ROOM_TABLE_NAME = "rooms";
     public static final String COLUMN_ROOMID = "roomid";
     public static final String COLUMN_ROOMNUMBER = "roomnumber";
@@ -31,11 +33,23 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String COLUMN_ROOMPRICE = "price";
     public static final String COLUMN_ROOMTYPE = "roomtype";
 
-//Thông số bảng rentlish
+    //Thông số bảng rentlist
     public static final String RENTLIST_TABLE_NAME = "rentlist";
     public static final String COLUMN_RENTID = "roomid";
     public static final String COLUMN_ENDDATE = "enddate";
 
+    //
+    public static final String ASSETS_TABLE_NAME = "asset";
+    public static final String COLUMN_ASSETID = "assetid";
+    public static final String COLUMN_HASAC = "hasac"; //có điều hòa
+    public static final String COLUMN_HASWH = "haswh"; //có nóng lạnh
+    public static final String COLUMN_HASWM = "haswm"; //có máy giặt
+
+    //
+    public static final String PROFILE_TABLE_NAME = "profile";
+    public static final String COLUMN_PROFILEID = "profileid";
+    public static final String COLUMN_SDT = "sdt";
+    public static final String COLUMN_EMAIL = "email";
 
 
     public DatabaseHelper(Context context) {
@@ -74,6 +88,27 @@ public class DatabaseHelper extends SQLiteOpenHelper {
                 + "CONSTRAINT fk2 FOREIGN KEY(" + COLUMN_ROOMNUMBER + ", " + COLUMN_AREA + ") "
                 + " REFERENCES " + ROOM_TABLE_NAME + " (" + COLUMN_ROOMNUMBER + ", " + COLUMN_AREA  + ")  )";
         db.execSQL(createTableQuery3);
+        //
+        String createTableQuery4 = "CREATE TABLE " + ASSETS_TABLE_NAME + "("
+                + COLUMN_ASSETID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + COLUMN_HASAC + " INTERGER, "
+                + COLUMN_HASWH + " INTERGER, "
+                + COLUMN_HASWM + " INTERGER, "
+                + COLUMN_ROOMNUMBER + " TEXT, "
+                + COLUMN_AREA + " TEXT, "
+                + "CONSTRAINT fk3 FOREIGN KEY(" + COLUMN_ROOMNUMBER + ", " + COLUMN_AREA + ") "
+                + " REFERENCES " + ROOM_TABLE_NAME + " (" + COLUMN_ROOMNUMBER + ", " + COLUMN_AREA  + ")  )";
+        db.execSQL(createTableQuery4);
+        //
+        String createTableQuery5 = "CREATE TABLE " + PROFILE_TABLE_NAME + "("
+                + COLUMN_PROFILEID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + COLUMN_USERNAME + " INTERGER, "
+                + COLUMN_SDT + " TEXT, "
+                + COLUMN_EMAIL + " TEXT, "
+                + "CONSTRAINT fk4 FOREIGN KEY(" + COLUMN_USERNAME + ") "
+                + " REFERENCES " + ACCOUNT_TABLE_NAME + " (" + COLUMN_USERNAME + ")  )";
+        db.execSQL(createTableQuery5);
+
     }
 
     @Override
@@ -105,6 +140,14 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public Cursor getRentByUsername(String username) {
         SQLiteDatabase db = getReadableDatabase();
         return db.query(RENTLIST_TABLE_NAME, null, COLUMN_USERNAME + " = ?", new String[]{username}, null, null, null);
+    }
+    public Cursor getAssetInfo(String roomnumber, String area) {
+        SQLiteDatabase db = getReadableDatabase();
+        return db.query(ASSETS_TABLE_NAME, null, COLUMN_ROOMNUMBER + " = ? AND " + COLUMN_AREA + " = ?", new String[]{roomnumber, area}, null, null, null);
+    }
+    public Cursor getProfileInfo(String username) {
+        SQLiteDatabase db = getReadableDatabase();
+        return db.query(PROFILE_TABLE_NAME, null, COLUMN_USERNAME + " = ?", new String[]{username}, null, null, null);
     }
 
 
@@ -138,5 +181,23 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         cv.put(COLUMN_ENDDATE, r.getEnddate());
         db.insert(RENTLIST_TABLE_NAME, null, cv);
         db.update(RENTLIST_TABLE_NAME, cv, COLUMN_USERNAME + " = ?", new String[] {r.getUsername()});
+    }
+    public void addAnAssetInfo(Asset a) {
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put(COLUMN_HASAC, a.getHasac());
+        cv.put(COLUMN_HASWH, a.getHaswh());
+        cv.put(COLUMN_HASWM, a.getHaswm());
+        cv.put(COLUMN_ROOMNUMBER, a.getRoomnumber());
+        cv.put(COLUMN_AREA, a.getRoomarea());
+        db.insert(ASSETS_TABLE_NAME, null, cv);
+    }
+    public void addAnProfile(Profile p){
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put(COLUMN_USERNAME, p.getUsername());
+        cv.put(COLUMN_SDT, p.getSdt());
+        cv.put(COLUMN_EMAIL, p.getEmail());
+        db.insert(PROFILE_TABLE_NAME, null, cv);
     }
 }
